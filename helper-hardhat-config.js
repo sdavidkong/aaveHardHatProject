db72c13f4a0ea27e784c22e8576a5e23febefac5
S David Kong <SDAVIDKONG@gmail.com>
2022-10-16 15:19:41 -0400

    added to getWeth script

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,55 +1,37 @@
 const networkConfig = {
-    default: {
-        name: "hardhat",
-        fee: "100000000000000000",
-        keyHash: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-        jobId: "29fa9aa13bf1468788b7cc4a500a45b8",
-        fundAmount: "1000000000000000000",
-        automationUpdateInterval: "30",
-    },
     31337: {
         name: "localhost",
-        fee: "100000000000000000",
-        keyHash: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-        jobId: "29fa9aa13bf1468788b7cc4a500a45b8",
-        fundAmount: "1000000000000000000",
-        automationUpdateInterval: "30",
-        ethUsdPriceFeed: "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
+        wethToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        lendingPoolAddressesProvider: "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+        daiEthPriceFeed: "0x773616E4d11A78F511299002da57A0a94577F1f4",
+        daiToken: "0x6b175474e89094c44da98b954eedeac495271d0f",
     },
-    1: {
-        name: "mainnet",
-        linkToken: "0x514910771af9ca656af840dff83e8264ecf986ca",
-        fundAmount: "0",
-        automationUpdateInterval: "30",
-    },
+    // Due to the changing testnets, this testnet might not work as shown in the video
     5: {
         name: "goerli",
-        linkToken: "0x326c977e6efc84e512bb9c30f76e30c160ed06fb",
         ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
-        keyHash: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
-        vrfCoordinator: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
-        oracle: "0xCC79157eb46F5624204f47AB42b3906cAA40eaB7",
-        jobId: "ca98366cc7314957b8c012c72f05aeeb",
-        fee: "100000000000000000",
-        fundAmount: "100000000000000000", // 0.1
-        automationUpdateInterval: "30",
+        wethToken: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
+        // This is the AaveV2 Lending Pool Addresses Provider
+        lendingPoolAddressesProvider: "0x5E52dEc931FFb32f609681B8438A51c675cc232d",
+        // This is LINK/ETH feed
+        daiEthPriceFeed: "0xb4c4a493AB6356497713A78FFA6c60FB53517c63",
+        // This is the LINK token
+        daiToken: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
     },
-    137: {
-        name: "polygon",
-        linkToken: "0xb0897686c545045afc77cf20ec7a532e3120e0f1",
-        ethUsdPriceFeed: "0xF9680D99D6C9589e2a93a78A04A279e509205945",
-        oracle: "0x0a31078cd57d23bf9e8e8f1ba78356ca2090569e",
-        jobId: "12b86114fa9e46bab3ca436f88e1a912",
-        fee: "100000000000000",
-        fundAmount: "100000000000000",
+    // Due to the different testnets, we are leaving kovan in as a reference
+    42: {
+        name: "kovan",
+        ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+        wethToken: "0xd0a1e359811322d97991e03f863a0c30c2cf029c",
+        lendingPoolAddressesProvider: "0x88757f2f99175387aB4C6a4b3067c77A695b0349",
+        daiEthPriceFeed: "0x22B58f1EbEDfCA50feF632bD73368b2FdA96D541",
+        daiToken: "0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD",
     },
 }
 
 const developmentChains = ["hardhat", "localhost"]
-const VERIFICATION_BLOCK_CONFIRMATIONS = 6
 
 module.exports = {
     networkConfig,
     developmentChains,
-    VERIFICATION_BLOCK_CONFIRMATIONS,
 }
--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -1,7 +1,18 @@
-const { getNamedAccounts } = require("hardhat")
+const { getNamedAccounts, ethers } = require("hardhat")
+const AMOUNT = ethers.utils.parseEther("0.02")
 
 async function getWeth() {
     const { deployer } = await getNamedAccounts()
+    const iWeth = await ethers.getContractAt(
+        "IWeth",
+        "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        deployer
+    )
+
+    await iWeth.deposit({ value: AMOUNT })
+    await tx.wait(1)
+    const wethBalance = await iWeth.balanceOF(deployer)
+    console.log(`Got ${wethBalance.toString()} WETH`)
 }
 
 module.exports = { getWeth }
